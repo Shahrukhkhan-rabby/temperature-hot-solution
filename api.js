974fe81d40e7deff14ed38efec55ab33d3021b3a
Md. Shahrukh Khan <shahrukhkhan3418198@gmail.com>
2024-08-19 08:58:43 +0600

    weather

--- api.js
+++ api.js
@@ -1,4 +1,4 @@
-const API_KEY = `46ad7457603b9b0104e633e78cd60e16`;
+const API_KEY = 'bf65cb0b705310d06ff3efab11573f2d';
 const searchTemperature = () => {
     const city = document.getElementById("city-name").value;
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
@@ -7,7 +7,21 @@ const searchTemperature = () => {
       .then((res) => res.json())
       .then((data) => displayTemperature(data));
 }
+const setInnerText = (id, text) => {
+    document.getElementById(id).innerText = text;
+}
 
 const displayTemperature = temperature => {
-    console.log(temperature);
-}
+    const city = temperature.name;
+    const tempKelvin = temperature.main.temp;
+    const tempCelsius = (tempKelvin - 273.15).toFixed(2);
+    const condition = temperature.weather[0].main;
+    const iconCode = temperature.weather[0].icon;
+    const iconUrl = `http://openweathermap.org/img/wn/${temperature.weather[0].icon}@2x.png`;
+
+    setInnerText('city', city);
+    setInnerText("temperature", `${tempCelsius}°C`);
+    setInnerText("condition", condition);
+    const imgIcon = document.getElementById('weather-icon');
+    imgIcon.setAttribute('src', iconUrl)
+};
---
+++ weather.js
@@ -0,0 +1,7 @@
+const apiKey = 'your_api_key_here';
+const city = 'Dhaka';
+
+function getWeatherData(city) {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q={city}&appid=${apiKey}&units=metric`;
+    
+}
